74c25c96cc83cdd1ed0eb138d4c9954a701a3c85
Dan Matthews <dan@danmatthews.me>
2023-10-29 22:55:44 +0000

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,18 +2,17 @@
 
 namespace Intrfce\LaravelFrontendEnums\Tests;
 
-use Illuminate\Foundation\Application;
-use Illuminate\Support\ServiceProvider;
 use Intrfce\LaravelFrontendEnums\LaravelFrontendEnumsServiceProvider;
 use Intrfce\LaravelFrontendEnums\Tests\Providers\TestApplicationServiceProvider;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
-class TestCase extends \Orchestra\Testbench\TestCase
+class TestCase extends OrchestraTestCase
 {
     /**
      * Get package providers.
      *
-     * @param  Application  $app
-     * @return array<int, class-string<ServiceProvider>>
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
      */
     protected function getPackageProviders($app): array
     {
